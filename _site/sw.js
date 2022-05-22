7faa1c971372d801b6fc404060ee54e6a2b2a224
heizes <jeroldcamacho.res@gmail.com>
2022-05-22 10:45:56 +0800

    Initial commit

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/blog/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220522.1023';
+const cacheName = 'chirpy-20220522.1045';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
