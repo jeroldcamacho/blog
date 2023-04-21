71c9e4ab59d553b85304c5421977dbc7a918faab
heizes <jeroldcamacho.res@gmail.com>
2023-04-21 17:21:27 +0800

    Update website

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/blog/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230421.1705';
+const cacheName = 'chirpy-20230421.1720';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
